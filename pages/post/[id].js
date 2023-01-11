31a392da1114f0a2d8fa262fde419a4adb4d2df4
caderitter <ritter.cade@gmail.com>
2023-01-11 15:54:57 -0600

    blocking

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -6,13 +6,18 @@ export async function getStaticPaths() {
   const posts = await getPosts({ limit: 100 });
 
   const paths = posts.map((post) => ({ params: { id: post?.sys?.id } }));
-  return { paths, fallback: false };
+  return { paths, fallback: blocking };
 }
 
 export async function getStaticProps({ params }) {
   const post = await getPost(params?.id);
-  const json = JSON.parse(JSON.stringify(post));
-  return { props: { post: json } };
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return { props: { post } };
 }
 
 function Image({ src, width, height, alt }) {
