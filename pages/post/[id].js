359e518a585474281a719825f569942844c0f66f
caderitter <ritter.cade@gmail.com>
2023-01-11 15:55:34 -0600

    oops

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -6,7 +6,7 @@ export async function getStaticPaths() {
   const posts = await getPosts({ limit: 100 });
 
   const paths = posts.map((post) => ({ params: { id: post?.sys?.id } }));
-  return { paths, fallback: blocking };
+  return { paths, fallback: "blocking" };
 }
 
 export async function getStaticProps({ params }) {
