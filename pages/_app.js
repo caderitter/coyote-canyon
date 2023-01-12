eb709a0d84985b846c70119f1b6d69f3cd544826
caderitter <ritter.cade@gmail.com>
2023-01-12 13:31:01 -0600

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Libre_Baskerville } from '@next/font/google';
+import '../styles/globals.css';
+
+const libreBaskerville = Libre_Baskerville({ weight: '400', subsets: 'latin' });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Coyote canyon</title>
+        <meta name="description" content="Sam Ritter's writing" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐺</text></svg>"
+        />
+      </Head>
+      <main className={libreBaskerville.className}>
+        <div className="container">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Libre_Baskerville } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-import { getPosts } from "../lib/posts";
-import Link from "next/link";
+import styles from '../styles/Home.module.css';
+import { getPosts } from '../lib/posts';
+import Link from 'next/link';
 
 export async function getStaticProps({ params }) {
   const skip = params?.skip;
@@ -11,39 +8,17 @@ export async function getStaticProps({ params }) {
   return { props: { posts } };
 }
 
-const libreBaskerville = Libre_Baskerville({ weight: "400", subsets: "latin" });
-
 export default function Home({ posts }) {
   return (
-    <div className={`${styles.container} ${libreBaskerville.className}`}>
-      <Head>
-        <title>Coyote canyon</title>
-        <meta name="description" content="Sam Ritter's writing" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.title}>
-              <Link href={`/post/${post.sys.id}`}>{post.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div className={styles.container}>
+      <header className={styles.header}>Sammie Ritter</header>
+      <ul className={styles.postList}>
+        {posts.map(post => (
+          <li className={styles.postItem} key={post.title}>
+            <Link href={`/post/${post.sys.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
